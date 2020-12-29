80f98b97c93b0956e738ffe247319c5803752a5c
LandRover <github.com@landroverz.com>
2020-12-29 17:07:11 +0200

    added useragent

--- src/triggers/url_to_image.js
+++ src/triggers/url_to_image.js
@@ -38,7 +38,7 @@ class URLtoImage {
                 launchOptions: { 
                     args: ['--no-sandbox', '--disable-setuid-sandbox']
                 },
-                userAgent: '',
+                userAgent: 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1',
                 scale: 0.5,
                 format: 'jpg'
             })
