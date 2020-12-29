9a3b0655d1f542228d4683a7847d003106699301
LandRover <github.com@landroverz.com>
2020-12-29 17:13:06 +0200

    removed scale

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ module.exports = {
                     args: ['--no-sandbox', '--disable-setuid-sandbox']
                 },
                 userAgent: 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1',
-                scale: 0.1,
+                scale: 1,
                 format: 'jpg',
             }
         }
